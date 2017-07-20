77f33004ed4996c2781dd2ac9cf15c9662191e94
垚淼 <zhenwei@zhenweideMacBook-Pro.local>
2017-07-20 11:46:18 +0800

    hah
    ahahahahhahhhh

---
+++ UIWebView-WKWebView-js 交互/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  UIWebView-WKWebView-js 交互
+//
+//  Created by 镇微 on 2017/7/18.
+//  Copyright © 2017年 镇微. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CommonHtml5Controller.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (IBAction)clickedBtn:(UIButton *)sender
+{
+    CommonHtml5Controller *htmlVC = [[CommonHtml5Controller alloc] init];
+    
+    htmlVC.htmlUrl = @"https://www.microtown.cn/appPage/recomPage1.jsp?fromAccount=9062477";
+    
+    [self.navigationController pushViewController:htmlVC animated:YES];
+}
+
+
+@end
